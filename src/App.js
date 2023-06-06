a68d87fe25ce02da87c106fce4f84b3ad5fd378e
dnqudgml12 <21800454@handomg.ac.kr>
2023-06-07 01:25:31 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactjhjhjkhkj
+          Learn Reactjhjhjkhkjfdsfds
         </a>
       </header>
     </div>
