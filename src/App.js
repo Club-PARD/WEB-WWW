a5ef6c96ee8f65fec3ae3b6b1bedc377436c8534
dnqudgml12 <21800454@handomg.ac.kr>
2023-06-18 22:28:04 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Inquiry from './pages/Inquiry';
+import Video1 from './pages/Video1';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+<Router>
+  <Routes>
+    <Route path= '/' element={<Home/>}/>
+    <Route path='/Video1' element={<Video1/>}/>
+    <Route path='/Inquiry' element={<Inquiry/>}/>
+    <Route path='About' element={<About/>}/>
 
-          Learn Reactjhjhjkhkjfdsfds
+  </Routes>
 
 
-        </a>
-      </header>
-    </div>
+</Router>
   );
 }
 
---
+++ src/pages/Inquiry.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Inquiry= ()=>{
+
+    return(
+        <>
+        <div>Inquiry</div>
+        </>
+
+    )
+}
+
+export default Inquiry;
