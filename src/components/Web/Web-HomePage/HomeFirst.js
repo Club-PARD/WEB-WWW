b43f264dc5064aa4bd803e67c73138e207e3b2c6
dnqudgml12 <21800454@handomg.ac.kr>
2023-06-19 14:12:21 +0900

    Aa

---
+++ src/components/Web/Web-HomePage/HomeFirst.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { useState,useEffect } from "react";
+import { styled } from "styled-components";
+
+
+
+const VideoContainer = styled.div`
+
+  position: sticky;
+  top:0;
+  width: 100%;
+  height: 100vh;
+  max-width: 100vw; 
+  min-width: 1440px; // 아무리 줄여도 1440px로 유지됨
+  background-color: ${({ isColor }) => (isColor ? ' rgba(0, 0, 0, 0.4)' : ' rgba(0, 0, 0, 0.4)')};
+`;
+const VideoBackground = styled.video`
+  width: 100%;
+  height: 100%;
+  object-fit: fill;
+  z-index: -1;
+`;
+
+//inline-block으로 stickybox를 위로 올리는 역할을한다.
+const DIVVVV = styled.div`
+  display: inline-block;
+  height: 1100px;
+  width: 100%;
+`;
+
+function useScrollPosition() {
+    const [scrollPos, setScrollPos] = useState(0);
+  
+    useEffect(() => {
+      const updateScrollPos = () => {
+        setScrollPos(window.pageYOffset);
+      };
+  
+      window.addEventListener('scroll', updateScrollPos);
+  
+      return () => {
+        window.removeEventListener('scroll', updateScrollPos);
+      };
+    }, []);
+  
+    return scrollPos;
+  }
+
+const HomeFirst=()=>{
+
+    const [isColor,setisColor]=useState(true);
+    const [backcolor, setbackcolor] = useState(true);// 배경색 조절
+
+
+    const position = useScrollPosition();
+
+
+    return(
+        <>
+        <DIVVVV>
+       <VideoContainer isColor={backcolor} >
+        <VideoBackground  autoPlay loop muted playsInline>
+      <source  playsInline muted autoPlay src={require("../../../Assets/Video/BackGroundVideo.mp4")} type="video/mp4" />
+    </VideoBackground> 
+    </VideoContainer>
+
+
+        </DIVVVV>
+        </>
+    )
+}
+
+
+export default HomeFirst;
--- src/components/Web/Web-HomePage/HomeSecond.js
+++ src/components/Web/Web-HomePage/HomeSecond.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { styled } from 'styled-components';
 
 const Div = styled.div`
 display: flex;
 justify-content: center;
-align-item: center;
+align-items: center;
 `;
 
 function HomeSecond() {
