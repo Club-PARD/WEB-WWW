7813f9218694efa6df5811c043338121c3fea75d
KJW519 <jwkang519@gmail.com>
2023-06-19 14:08:59 +0900

    121

---
+++ src/components/Web/Web-HomePage/HomeSecond.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Div = styled.div`
+display: flex;
+justify-content: center;
+align-item: center;
+`;
+
+function HomeSecond() {
+    return (
+        <Div>
+            Theme Choose
+        </Div>
+    );
+}
+
+export default HomeSecond;
