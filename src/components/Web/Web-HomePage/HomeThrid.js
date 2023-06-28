46f42661a601aff54ee323a1cf651f0b463843da
shyoon840 <shyoon840@handong.ac.kr>
2023-06-28 15:32:16 +0900

    editing time check box

--- src/components/Web/Web-HomePage/HomeThrid.js
+++ src/components/Web/Web-HomePage/HomeThrid.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -25,55 +25,65 @@ const PartDiv = styled.div`
 `;
 
 const Box = styled.div`
-  width: 442px;
-  height: 535px;
-  margin-right: 27px;
-  cursor: pointer;
+  width: 355px;
+  height: 500px;
+  flex-shrink: 0;
+  border-radius: 20px;
+  background: url(<path-to-image>), lightgray 50% / cover no-repeat;
+  background-color: transparent;
+  box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.10), 8px 13px 34px 0px rgba(0, 0, 0, 0.10), 33px 53px 62px 0px rgba(0, 0, 0, 0.09), 74px 119px 84px 0px rgba(0, 0, 0, 0.05), 132px 212px 100px 0px rgba(0, 0, 0, 0.01), 207px 331px 109px 0px rgba(0, 0, 0, 0.00);
+  margin-right: 111px;
+  display: flex; 
+  justify-content: center; /* 수평 가운데 정렬 */
+  align-items: center; /* 수직 가운데 정렬 */
+`;
+
+const TimeText = styled.a`
+  color: var(--text, #323338);
+  text-align: center;
+  font-size: 48px;
+  font-family: NanumBarunGothic;
+  font-weight: 600;
+  line-height: 140%;
 `;
 
 const TimeA = styled(Box)`
-  background-color: lightblue;
+  margin-left: 77px;
 `;
 
 const TimeB = styled(Box)`
-  background-color: lightgreen;
 `;
 
 const TimeC = styled(Box)`
-  background-color: lightgreen;
 `;
 
-const HomeThrid = ({ setTime }) => {
-
-
+const HomeThird = ({ setTime }) => {
   const handleOptionChange = (duration) => {
     setTime(duration);
   };
 
   return (
     <Container>
-      <TimeTitle>시간을 선택해주세요</TimeTitle>
+      <TimeTitle>쉬고 싶은 테마를 골라주세요.</TimeTitle>
       <PartDiv>
-        <Link
-          to="/Video1"
-          onClick={() => handleOptionChange(15)}
-        >
-          <TimeA >15분 선택</TimeA>
+        <Link to="/Video1" style={{ textDecoration: 'none' }} onClick={() => handleOptionChange(15)}>
+          <TimeA>
+            <TimeText>15분 선택</TimeText>
+          </TimeA>
         </Link>
-        <Link to="/Video1"
-          onClick={() => handleOptionChange(20)}
-        >
-          <TimeB >30분 선택</TimeB>
+        <Link to="/Video1" style={{ textDecoration: 'none' }} onClick={() => handleOptionChange(30)}>
+          <TimeB>
+            <TimeText>30분 선택</TimeText>
+          </TimeB>
         </Link>
-        <Link to="/Video1"
-          onClick={() => handleOptionChange(30)}
-
-        >
-          <TimeC >무제한</TimeC>
+        <Link to="/Video1" style={{ textDecoration: 'none' }} onClick={() => handleOptionChange(0)}>
+          <TimeC>
+            <TimeText>무제한</TimeText>
+          </TimeC>
         </Link>
       </PartDiv>
     </Container>
   );
 };
 
-export default HomeThrid;
+export default HomeThird;
