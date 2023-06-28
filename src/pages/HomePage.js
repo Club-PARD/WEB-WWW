e8f5e5458f2291caa75671b080527eb9186ad4c3
KJW519 <jwkang519@gmail.com>
2023-06-29 04:03:31 +0900

    12321

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { styled } from "styled-components";
 import HomeFirst from "../components/Web/Web-HomePage/Web-HomeFirst";
 import HomeThrid from "../components/Web/Web-HomePage/Web-HomeThrid";
+import HomeSecond from "../components/Web/Web-HomePage/Web-HomeSecond";
 import Hamburgerhome from "../components/Web/Web-HomePage/Web-Hamburgerhome";
 
 const Home = ({ setUser, setTime }) => { // setTime을 props로 받아옵니다.
@@ -11,7 +12,7 @@ const Home = ({ setUser, setTime }) => { // setTime을 props로 받아옵니다.
             <Hamburgerhome setUser={setUser} />
             <HomeFirst setUser={setUser} />
             {/* <HomeSecond /> */}
-            <HomeThrid setTime={setTime} />
+            {/* <HomeThrid setTime={setTime} /> */}
         </>
     );
 }
