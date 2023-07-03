3273061837587daa110f4500e04fb0ce8f08a609
shyoon840 <shyoon840@handong.ac.kr>
2023-07-03 09:50:53 +0900

    editing mobile video

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import HomePage from "./pages/HomePage";
 import InquiryPage from "./pages/InquiryPage";
 import Mypage from "./pages/Mypage";
 import OtherpageHomePage from "./pages/OtherpageHomePage";
-
+import Video from "./pages/VideoPage";
 import ForstVideo from "./components/Web/Web-VideoPage/Web-ForstVideo";
 import Writing from "./pages/WritingPage";
 
@@ -30,65 +30,16 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <HomePage
-              setUser={setUser}
-              setTime={setTime}
-              setTheme={setTheme}
-              theme={theme}
-            />
-          }
-        />
-        <Route
-          path="/Video"
-          element={<ForstVideo setUser={setUser} time={time} />}
-        />
+        <Route path="/" element={ <HomePage setUser={setUser} setTime={setTime} setTheme={setTheme} theme={theme} /> }/>
+        <Route path="/Video" element={<Video setUser={setUser} time={time} />} />
         <Route path="/Inquiry" element={<InquiryPage />} />
         <Route path="/About" element={<AboutPage setUser={setUser} />} />
         {/* 로그인한 사용자만 접근하도록 하였다 아니면 홈페이지로 렌더링 */}
         <Route path="/Using" element={<Usingway setUser={setUser} />} />
-        <Route
-          path="/Mypage"
-          element={
-            user ? (
-              <Mypage setUser={setUser} user={user} />
-            ) : (
-              <Navigate to="/" replace />
-            )
-          }
-        />
-        <Route
-          path="/Otherpage"
-          element={
-            user ? (
-              <OtherpageHomePage user={user} />
-            ) : (
-              <Navigate to="/" replace />
-            )
-          }
-        />
-        <Route
-          path="/Community"
-          element={
-            user ? (
-              <CommunityAllPage setUser={setUser} user={user} />
-            ) : (
-              <Navigate to="/" replace />
-            )
-          }
-        />
-        <Route
-          path="/Writing"
-          element={
-            user ? (
-              <Writing setUser={setUser} user={user} />
-            ) : (
-              <Navigate to="/" replace />
-            )
-          }
-        />
+        <Route path="/Mypage" element={user ?(<Mypage setUser={setUser} user={user}/>):(<Navigate to="/" replace />)}/>
+        <Route path="/Otherpage" element={user ? (<OtherpageHomePage user={user} />) : (<Navigate to="/" replace />)}/>
+        <Route path="/Community" element={user ? (<CommunityAllPage setUser={setUser} user={user}/>) : (<Navigate to="/" replace />)}/>
+        <Route path="/Writing" element={ user ? (<Writing setUser={setUser} user={user} />) : (<Navigate to="/" replace />)}/>
       </Routes>
     </Router>
   );
