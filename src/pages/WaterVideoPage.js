446ff0d5e343d724674da3a17e9b4ee11748e2f3
shyoon840 <shyoon840@handong.ac.kr>
2023-07-03 12:39:11 +0900

    동영상 페이지 생성

---
+++ src/pages/WaterVideoPage.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import { useMediaQuery } from "react-responsive";
+import Hamburgerhome from "../components/Web/Web-HomePage/Web-Hamburgerhome";
+import HamburgerMob from "../components/Mobile/HomePage_Mob/Mob-Hamburger";
+import ForstVideoWeb from "../components/Web/Web-VideoPage/Web-WaterVideo";
+import ForstVideoMob from "../components/Mobile/VideoPage_Mob/Mob-WaterVideo";
+
+const Video = ({ setUser, user }) => {
+  const isDesktopOrMobile = useMediaQuery({ query: "(max-width:768px)" });
+  const AboutPageComponent = styled.div``;
+
+  return (
+    <AboutPageComponent>
+      {isDesktopOrMobile !== true ? (
+        <>
+          <ForstVideoWeb setUser={setUser}/>
+        </>
+      ) : (
+        <>
+          <ForstVideoMob setUser={setUser}/>
+          {/* <HamburgerMob setUser={setUser} /> */}
+        </>
+      )}
+    </AboutPageComponent>
+  );
+};
+
+export default Video;
